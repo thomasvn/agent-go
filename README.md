82559c981bc8d45273ef8ed99cf6871eb273bed4
thomasvn <thomasnguyen96@gmail.com>
2025-04-18 22:11:18 -0700

    Add tools

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 
 Inspired by <https://ampcode.com/how-to-build-an-agent>
 
-<!-- 
+<!--
 TODO:
-- Finish blog post
+- Try integrating mcp servers with the agent?
+- Try integrating with local Ollama models?
 -->
 
-<!-- 
+<!--
 DONE:
-
+- Finish tutorial
 -->
--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
+
+	"agent/pkg/tool"
 
 	"github.com/anthropics/anthropic-sdk-go"
 )
@@ -20,7 +23,8 @@ func main() {
 		return scanner.Text(), true
 	}
 
-	agent := NewAgent(&client, getUserMessage)
+	tools := []tool.ToolDefinition{tool.ReadFileDefinition, tool.ListFilesDefinition, tool.EditFileDefinition}
+	agent := NewAgent(&client, getUserMessage, tools)
 	err := agent.Run(context.TODO())
 	if err != nil {
 		fmt.Printf("Error: %s\n", err.Error())
@@ -28,14 +32,16 @@ func main() {
 }
 
 type Agent struct {
-	client *anthropic.Client
+	client         *anthropic.Client
 	getUserMessage func() (string, bool)
+	tools          []tool.ToolDefinition
 }
 
-func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool)) *Agent {
+func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool), tools []tool.ToolDefinition) *Agent {
 	return &Agent{
-		client: client,
+		client:         client,
 		getUserMessage: getUserMessage,
+		tools:          tools,
 	}
 }
 
@@ -44,15 +50,18 @@ func (a *Agent) Run(ctx context.Context) error {
 
 	fmt.Println("Chat with Claude (use 'ctrl-c' to quit)")
 
+	readUserInput := true
 	for {
-		fmt.Print("\u001b[94mYou\u001b[0m: ")
-		userInput, ok := a.getUserMessage()
-		if !ok {
-			break
-		}
+		if readUserInput {
+			fmt.Print("\u001b[94mYou\u001b[0m: ")
+			userInput, ok := a.getUserMessage()
+			if !ok {
+				break
+			}
 
-		userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
-		conversation = append(conversation, userMessage)
+			userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
+			conversation = append(conversation, userMessage)
+		}
 
 		message, err := a.runInference(ctx, conversation)
 		if err != nil {
@@ -60,22 +69,66 @@ func (a *Agent) Run(ctx context.Context) error {
 		}
 		conversation = append(conversation, message.ToParam())
 
+		toolResults := []anthropic.ContentBlockParamUnion{}
 		for _, content := range message.Content {
 			switch content.Type {
 			case "text":
 				fmt.Printf("\u001b[93mClaude\u001b[0m: %s\n", content.Text)
+			case "tool_use":
+				result := a.executeTool(content.ID, content.Name, content.Input)
+				toolResults = append(toolResults, result)
 			}
 		}
+		if len(toolResults) == 0 {
+			readUserInput = true
+			continue
+		}
+		readUserInput = false
+		conversation = append(conversation, anthropic.NewUserMessage(toolResults...))
 	}
 
 	return nil
 }
 
 func (a *Agent) runInference(ctx context.Context, conversation []anthropic.MessageParam) (*anthropic.Message, error) {
+	anthropicTools := []anthropic.ToolUnionParam{}
+	for _, tool := range a.tools {
+		anthropicTools = append(anthropicTools, anthropic.ToolUnionParam{
+			OfTool: &anthropic.ToolParam{
+				Name:        tool.Name,
+				Description: anthropic.String(tool.Description),
+				InputSchema: tool.InputSchema,
+			},
+		})
+	}
+
 	message, err := a.client.Messages.New(ctx, anthropic.MessageNewParams{
-		Model: anthropic.ModelClaude3_7SonnetLatest,
+		Model:     anthropic.ModelClaude3_7SonnetLatest,
 		MaxTokens: int64(1024),
-		Messages: conversation,
+		Messages:  conversation,
+		Tools:     anthropicTools,
 	})
 	return message, err
+}
+
+func (a *Agent) executeTool(id, name string, input json.RawMessage) anthropic.ContentBlockParamUnion {
+	var toolDef tool.ToolDefinition
+	var found bool
+	for _, tool := range a.tools {
+		if tool.Name == name {
+			toolDef = tool
+			found = true
+			break
+		}
+	}
+	if !found {
+		return anthropic.NewToolResultBlock(id, "tool not found", true)
+	}
+
+	fmt.Printf("\u001b[92mtool\u001b[0m: %s(%s)\n", name, input)
+	response, err := toolDef.Function(input)
+	if err != nil {
+		return anthropic.NewToolResultBlock(id, err.Error(), true)
+	}
+	return anthropic.NewToolResultBlock(id, response, false)
 }
